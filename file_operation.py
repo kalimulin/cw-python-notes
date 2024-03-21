980df23e919e3c0b94c2a757f605471f51e75bb2
Foxycoder <r.kalimulin@jetmail.cc>
2024-03-21 11:07:27 +0400

    приложение для заметок

---
+++ file_operation.py
@@ -0,0 +1,32 @@
+import os
+import Note
+
+
+def write_file(array, mode):
+    if os.path.isfile('notes.csv'):
+        with open('notes.csv', 'w') as file:
+            for notes in array:
+                file.write(Note.Note.to_string(notes))
+                file.write('\n')
+            file.close()
+
+    else:
+        print('Файл не найден')
+
+
+def read_file():
+    array = []
+    try:
+        file = open("notes.csv", "r", encoding='utf-8')
+        notes = file.read().strip().split("\n")
+        if len(notes) > 0:
+            for n in notes:
+                split_n = n.split(';')
+                note = Note.Note(id=split_n[0], title=split_n[1], body=split_n[2], date=split_n[3])
+                array.append(note)
+        else:
+            print('Нет сохраненных заметок.')
+    except OSError:
+        print('Файл недоступен для чтения')
+    finally:
+        return array
